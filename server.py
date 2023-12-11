0c07aa790312dd1b6ab24e6186675378cd0e02e9
Aaron Johnston <aaron.johnston001@gmail.com>
2023-12-11 01:16:43 -0800

    HTMX updating list working

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Form, Request
 from typing import Annotated
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import HTMLResponse, PlainTextResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from llama_cpp import Llama
@@ -13,11 +13,15 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 
 templates = Jinja2Templates(directory='templates')
 
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
 @app.get('/healthcheck', response_class=PlainTextResponse)
 async def healthcheck():
     return 'working'
 
-@app.post('/llm', response_class=PlainTextResponse)
+@app.post('/llm', response_class=HTMLResponse)
 async def query_llm(request: Request, input: Annotated[str, Form()]):
     output = llm(
         input,
