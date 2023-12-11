72e9c8be5cef25cdb0f1a250b61ca4dab1c3fcb9
Aaron Johnston <aaron.johnston001@gmail.com>
2023-12-11 00:15:33 -0800

    First successful test of local Llama within server

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from starlette.responses import FileResponse
+from llama_cpp import Llama
+
+llm = Llama(model_path='./models/codellama-7b.Q4_K_M.gguf')
 
 app = FastAPI()
 
@@ -12,6 +15,9 @@ async def index():
 async def test():
     return 'working'
 
-@app.get('/llm', response_class=PlainTextResponse)
+@app.get('/llm')
 async def test():
-    return 'LLM Transmute Test'
+    return llm(
+        'Q: What is the peak of an orbit called? A:',
+        max_tokens=32
+    )
