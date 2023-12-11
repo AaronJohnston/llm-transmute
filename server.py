69573de55c452a30b4250289978434679a1be18d
Aaron Johnston <aaron.johnston001@gmail.com>
2023-12-10 23:50:25 -0800

    Update gitignore to ignore models

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
 @app.get('/')
-async def root():
-    return {'message': 'sup'}
+async def index():
+    return FileResponse('index.html')
 
-@app.get('/test')
+@app.get('/healthcheck', response_class=PlainTextResponse)
+async def test():
+    return 'working'
+
+@app.get('/llm', response_class=PlainTextResponse)
 async def test():
     return 'LLM Transmute Test'
